d84c3c3fcc27d1c8ae0c9ca4021aca5c51bb1f0c
Jousesss <aleksei.burilov@gmail.com>
2024-01-19 18:58:17 +0300

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+class myClasses {
+    public static void main(String[] args) throws IOException {
+
+        int firstNumber = 5;
+        System.out.println(firstNumber + " это цифра!");
+        int secondNumber = 15;
+
+    }
+}
+
